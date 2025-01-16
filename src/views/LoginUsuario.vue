28eed49d15f23ba756633a5261b85c55fccb9f3e
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-01-16 18:03:59 -0300

    Update label

--- src/views/LoginUsuario.vue
+++ src/views/LoginUsuario.vue
@@ -8,11 +8,16 @@
                 <p class="text-muted mb-5">Por favor, insira seus dados.</p>
                 <form>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email</label>
+                        
+                        <div class="d-flex justify-content-start">
+                            <label for="email" class="form-label">Email</label>
+                        </div>
                         <input type="email" class="form-control" id="email" placeholder="Digite aqui" />
                     </div>
                     <div class="mb-3">
-                        <label for="password" class="form-label">Senha</label>
+                        <div class="d-flex justify-content-start">
+                            <label for="password" class="form-label">Senha</label>
+                        </div>
                         <input type="password" class="form-control" id="password" placeholder="********" />
                     </div>
                     <div class="d-flex justify-content-between align-items-center mb-3">
