903fcf4aa1bdeea659158813e08d1dd4c2bee1cd
mateusmaranhao12 <mateusnmaranhao@gmail.com>
2025-01-17 12:17:57 -0300

    Implement ver-todas and time

--- src/views/MenuUsuario.vue
+++ src/views/MenuUsuario.vue
@@ -16,7 +16,33 @@
 
             <!-- Conteúdo Principal -->
             <div class="col-md-11">
-                <h1>a</h1>
+                <div class="row d-flex align-items-center justify-content-between flex-wrap">
+
+                    <!-- Conteúdo Time e Ver Todos -->
+                    <div class="d-flex align-items-center flex-wrap w-100">
+                        <!-- Ver Todos -->
+                        <div class="ver-todos d-flex align-items-center me-3 flex-wrap">
+                            <i class="fa-solid fa-house me-3"></i>
+                            <i class="fa-solid fa-arrow-right me-3"></i>
+                            <span class="me-3">Chats</span>
+                            <i class="fa-solid fa-arrow-right me-3"></i>
+                            <span class="me-3">Ver todos</span>
+                            <i class="fa-solid fa-arrow-right me-3"></i>
+                            <span style="background-color: rgb(248, 248, 248); border-radius: 10px; padding: 10px;">
+                                Mateus Maranhão
+                            </span>
+                        </div>
+
+                        <!-- Time -->
+                        <div class="time d-flex align-items-center justify-content-center flex-wrap">
+                            <span class="me-3">Ramal: 100001: Livre</span>
+                            <span class="me-3">Tempo logado: 0-16:14:43</span>
+                            <span class="me-3">Última Chamada: ----------</span>
+                            <span class="me-3">Disponível: 00:00:28</span>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
